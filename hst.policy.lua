0ef0a8a9b325c3f78164ef74ac833a110a18e070
Zoka Agari <zokaagari@proton.me>
2024-06-10 19:06:38 +1000

    feat: cron handler to fetch candles

--- hst.policy.lua
+++ hst.policy.lua
@@ -1,3 +1,9 @@
+local ao = require('ao')
+local json = require('json')
+
+local _DEXI = 'jao0bfwk99iME8aK_TJLjm8H0bwaHzNuVbKRE1jArRo'
+local _AMM = '2bKo3vwB1Mo5TItmxuUQzZ11JgKauU_n2IZO1G13AIk'
+
 --[[
   This module gets the highest low and acts on the monetary policy for HST
 ]]
@@ -6,6 +12,19 @@
 ---@type number
 HighestLow = HighestLow or 0
 
+--[[
+     Get most recent candle information for HST
+   ]]
+--
+Handlers.add('get-candles', Handlers.utils.hasMatchingTag('Action', 'Cron'), function(msg)
+  ao.send({
+    Target = _DEXI,
+    Action = 'Get-Candles',
+    ['Days'] = '4',
+    ['Interval'] = '1d',
+    ['AMM'] = _AMM
+  })
+end)
 
 --[[
   Get the highest low from the daily candles
