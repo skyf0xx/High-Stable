b943831778304bbd54cf608b227d2d313ce890bb
Zoka Agari <zokaagari@proton.me>
2024-06-29 13:12:28 +1000

    feat: add stake handler

---
+++ mithril.stake.mint.lua
@@ -0,0 +1,83 @@
+Variant = '0.0.1'
+local bint = require('.bint')(256)
+local tableUtils = require('.utils')
+local allowedTokens = { stETH = 'xxxx', stSOL = 'yyy' }
+
+
+--[[
+  Initialize the staker table. stakers[token][user] = balance
+]]
+---@return table<string, table>
+function InitStakers()
+  local stakers = {}
+  for _, token in pairs(allowedTokens) do
+    stakers[token] = {}
+  end
+  return stakers
+end
+
+Stakers = Stakers or InitStakers()
+
+
+--[[
+  utils helper functions to remove the bint complexity.
+]]
+--
+local utils = {
+  add = function(a, b)
+    return tostring(bint(a) + bint(b))
+  end,
+  subtract = function(a, b)
+    return tostring(bint(a) - bint(b))
+  end,
+  toBalanceValue = function(a)
+    return tostring(bint(a))
+  end,
+  toNumber = function(a)
+    return tonumber(a)
+  end
+}
+
+
+--[[
+     Handler for staking. To stake, simply send tokens to this address.
+   ]]
+--
+
+---@param address string
+---@return string
+function TokenName(address)
+  for token, addr in pairs(allowedTokens) do
+    if addr == address then
+      return token
+    end
+  end
+
+  return ''
+end
+
+Handlers.add('stake', Handlers.utils.hasMatchingTag('Action', 'Credit-Notice'),
+  function(msg)
+    -- credit notice is sent by the token process to the staking contract
+    local token = msg.From
+    local quantity = msg.Quantity
+    local stakeable = tableUtils.includes(token, tableUtils.values(allowedTokens))
+    local sender = msg.Sender
+    local tokenName = TokenName(token)
+
+    --don't bother to refund unstakeable tokens - to prevent being drained through spurious fees
+    assert(type(stakeable) == true, 'Token: ' .. token .. ' is not stakable and was ignored!')
+    assert(bint(0) < bint(quantity), 'Quantity must be greater than zero!')
+
+    if not Stakers[token][sender] then Stakers[token][sender] = '0' end
+
+    Stakers[token][sender] = utils.add(Stakers[token][sender], quantity)
+
+    ao.send({
+      Target = sender,
+      Data = Colors.gray ..
+        'You have staked a total of ' ..
+        Colors.blue .. Stakers[token][sender] .. Colors.reset .. ' ' .. tokenName
+    })
+  end)
+
