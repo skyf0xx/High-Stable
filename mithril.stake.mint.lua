7cb2ca424eeb864196f8378c1973c3ec51002030
Zoka Agari <zokaagari@proton.me>
2024-12-18 10:44:08 +1100

    feat: add new tokens

--- mithril.stake.mint.lua
+++ mithril.stake.mint.lua
@@ -32,7 +32,10 @@ local allowedTokens = {
   war = 'xU9zFkq3X2ZQ6olwNVvr1vUWIjc3kXTWr7xKQD6dh10',
   frp = 'NgPF445swQfqq0cuM1nNQaVzg05sq8eX5L4YxS-6oDo',
   qar_nab_lp = 'NX9PKbLVIyka3KPZghnEekw9FB2dfzbzVabpY-ZN1Dg',
+  war_nab_lp = '9eM72ObMJM6o3WHi6nTldwhHsCXSKgzz1hv-FpURZB4',
   wusdc_nab_lp = 'bxpz3u2USXv8Ictxb0aso3l8V9UTimaiGp9henzDsl8',
+  nab_war_ps_lp = 'BGBUvr5dVJrgmmuPN6G56OIuNSHUWO2y7bZyPlAjK8Q',
+  qar_nab_ps_lp = '230cSNf7AWy6VsBTftbTXW76xR5H1Ki42nT2xM2fA6M',
 }
 local allowedTokensNames = {
   agent_qar_lp = 'Botega LP qAR/AGENT',
@@ -40,8 +43,13 @@ local allowedTokensNames = {
   war = 'wAR',
   frp = 'Fren Points',
   qar_nab_lp = 'Botega LP qAR/NAB',
+  war_nab_lp = 'Botega LP wAR/NAB',
   wusdc_nab_lp = 'Botega LP wUSDC/NAB',
+  nab_war_ps_lp = 'Permaswap LP NAB/wAR',
+  qar_nab_ps_lp = 'Permaswap LP qAR/NAB',
 }
+
+-- weight forumula for lp: 2*(1/ total lp tokens per 1AR)
 local tokenWeights = {
   agent_qar_lp = '1',
   qar = '2600',
