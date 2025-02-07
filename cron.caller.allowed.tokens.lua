9355aa85473059feb47e946fb54ae0ef8aede05a
Zoka Agari <zokaagari@proton.me>
2025-02-08 07:35:51 +1100

    feat: secure crons

--- cron.caller.allowed.tokens.lua
+++ cron.caller.allowed.tokens.lua
@@ -1,6 +1,13 @@
 ALLOWED_TOKENS = 'G3biaSUvclo3cd_1ErpPYt-VoSSazWrKcuBlzeLkTnU'
+
+local function isCron(msg)
+  return msg.Action == 'Cron' and (msg.From == ao.env.Process.Owner or msg.From == ao.id)
+end
+
 Handlers.add('request-token-mints', Handlers.utils.hasMatchingTag('Action', 'Cron'),
   function(msg)
+    assert(isCron(msg), 'message is not from a trusted process')
+
     -- Send mint requests to token contract
     Send({
       Target = ALLOWED_TOKENS,
--- cron.caller.stats.lua
+++ cron.caller.stats.lua
@@ -1,6 +1,13 @@
 STATS = 'dNmk7_vhghAG06yFnRjm0IrFKPQFhqlF0pU7Bk3RmkM'
+
+local function isCron(msg)
+  return msg.Action == 'Cron' and (msg.From == ao.env.Process.Owner or msg.From == ao.id)
+end
+
+
 Handlers.add('request-update-stats', Handlers.utils.hasMatchingTag('Action', 'Cron'),
   function(msg)
+    assert(isCron(msg), 'message is not from a trusted process')
     -- Send mint requests to token contract
     Send({
       Target = STATS,
